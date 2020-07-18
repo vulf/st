97694e7052d9e0544f860c5b6045588a7e6135b1
vulf <pranavsivvam@gmail.com>
2020-07-18 21:04:28 +0530

    changed font and cursor

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Fira Mono:Medium:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 20;
 
 /*
@@ -139,7 +139,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default columns and rows numbers
